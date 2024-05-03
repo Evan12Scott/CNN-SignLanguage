a8aacc34207cfcd848f2617b81d696bc089496aa
Kieran Kennedy <kierankennedy@sandiego.edu>
2024-05-03 13:11:01 -0700

    Added simple test file

---
+++ TestCNN.py
@@ -0,0 +1,18 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from PIL import Image
+
+model = keras.models.load_model("ASLmodel.keras")
+key = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+im = Image.open("l.png")
+im.convert("RGB")
+
+im_arr = np.array(im)
+im_arr = np.divide(im_arr, 255)
+im_arr = im_arr[:,:,0:1]
+im_arr = np.reshape(im_arr, (1, 28, 28))
+
+predicition = model.predict(im_arr)
+print(key[np.argmax(predicition)])
