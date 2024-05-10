9e59878831a040ec1fb056b14711d9b4f37c99a6
Evan Scott <evan.scott@dc.rr.com>
2024-05-09 18:00:42 -0700

    fixed testcnn.py file path

--- TestCNN.py
+++ TestCNN.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from tensorflow import keras
 from PIL import Image
 
-model = keras.models.load_model("ASLmodel.keras")
+model = keras.models.load_model("./Models/results.keras")
 key = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 im = Image.open("l.png")
@@ -14,5 +14,5 @@ im_arr = np.divide(im_arr, 255)
 im_arr = im_arr[:,:,0:1]
 im_arr = np.reshape(im_arr, (1, 28, 28))
 
-predicition = model.predict(im_arr)
-print(key[np.argmax(predicition)])
+prediction = model.predict(im_arr)
+print(key[np.argmax(prediction)])
