15e7af7f30db104664c53f4da2d1543679f1a2f7
Evan Scott <evan.scott@dc.rr.com>
2024-05-12 14:45:32 -0700

    fixed overfit model error

--- ASL_CNN_Fit.py
+++ ASL_CNN_Fit.py
@@ -15,12 +15,14 @@ class Fit_Model:
         model.add(layers.MaxPooling2D((2, 2)))
         model.add(layers.Conv2D(64, (3, 3), activation='relu'))
         model.add(layers.MaxPooling2D((2, 2)))
-        model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(layers.Conv2D(128, (3, 3), activation='relu'))
         model.add(layers.Flatten())
+        model.add(layers.Dense(256, activation='relu'))
+        model.add(layers.Dropout(0.5))
         model.add(layers.Dense(128, activation='relu'))
         model.add(layers.Dropout(0.5))
         model.add(layers.Dense(26, activation="softmax"))
-
+        
         #Compile and train
         model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
         model.fit(train_data, train_labels, epochs=10, validation_data=(test_data, test_labels))
